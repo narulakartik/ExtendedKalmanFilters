60307cf310e52fc48bf0c688f5bf03eb1dbdddf5
narulakartik <kartiknarula@cmu.edu>
2020-05-11 19:20:33 +0000

    new edits 5/11

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -47,7 +47,7 @@ FusionEKF::FusionEKF() {
   		  0,1,0,1,
   		 0,0,1000,0,
   		0,0,0,1000;
-   Hj_=tools.CalculateJacobian(ekf_.x_);
+  
   
     	
   //P<<1,0,0,0,
@@ -98,7 +98,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       //         and initialize state.
       float rho=measurement_pack.raw_measurements_[0];
       float phi=measurement_pack.raw_measurements_[1];
-      float rhodot=measurement_pack.raw_measurements_[2];
+    //  float rhodot=measurement_pack.raw_measurements_[2];
       float x=rho*cos(phi);
       float y=rho*sin(phi);
       ekf_.x_<<x,y,0,0;
@@ -132,8 +132,8 @@ previous_timestamp_ = measurement_pack.timestamp_;
   
   
   ekf_.F_<<1,0,dt,0,
-  0,1,0,dt;
-  0,0,1,0;
+  0,1,0,dt,
+  0,0,1,0,
   0,0,0,1;
   
   //ekf_.F_(0,2)=dt;
@@ -141,7 +141,7 @@ previous_timestamp_ = measurement_pack.timestamp_;
   float dt_2 = dt * dt;
   float dt_3 = dt_2 * dt;
   float dt_4 = dt_3 * dt;
-  MatrixXd q;
+
  
   ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
          0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
@@ -164,16 +164,19 @@ previous_timestamp_ = measurement_pack.timestamp_;
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-    
-     ekf_.Init(ekf_.x_, ekf_.P_, ekf_.F_, Hj_, R_radar_, q);
+     Hj_=tools.CalculateJacobian(ekf_.x_);
+     ekf_.H_=Hj_;
+    ekf_.R_=R_radar_;
+     ekf_.Init(ekf_.x_, ekf_.P_, ekf_.F_, ekf_.H_, ekf_.R_, ekf_.Q_);
     
     
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
     // TODO: Laser updates
-    
-    ekf_.Init(ekf_.x_, ekf_.P_, ekf_.F_, H_laser_, R_laser_, q);
+    ekf_.H_=H_laser_;
+    ekf_.R_=R_laser_;
+    ekf_.Init(ekf_.x_, ekf_.P_, ekf_.F_, ekf_.H_, ekf_.R_, ekf_.Q_);
     
     ekf_.Update(measurement_pack.raw_measurements_);
     
